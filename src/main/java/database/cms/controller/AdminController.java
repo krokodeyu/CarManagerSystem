5e2a63fd1a7d29e846cf7c0937074bed21b4b1ca
Liang Ruizhe <23302010058@m.fudan.edu.cn>
2025-05-26 16:07:06 +0800

    update：更新controller框架

---
+++ src/main/java/database/cms/controller/AdminController.java
@@ -0,0 +1,18 @@
+package database.cms.controller;
+
+import database.cms.service.AdminService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RestController
+@RequestMapping("api/admin")
+public class AdminController {
+
+    private final AdminService adminService;
+
+    public AdminController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+}
---
+++ src/main/java/database/cms/service/AdminService.java
@@ -0,0 +1,7 @@
+package database.cms.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdminService {
+}
